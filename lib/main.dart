d459119786913ad79742736db3ff3038d90b10d5
tamas-polgar <tamas.polgar.dev@gmail.com>
2019-09-25 16:14:10 +0300

    changed font and finished chat list

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 			theme: ThemeData(
 				brightness: Brightness.dark,
 				primarySwatch: Colors.blue,
-				fontFamily: 'RobotoMono',
+				fontFamily: 'Oxygen',
 			),
 			home: MyHomePage(),
 		);
@@ -30,6 +30,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 	
 	ScrollController scrollController = new ScrollController();
+	
+	ScrollController chatListSrollController = new ScrollController();
 	
 	List<String> lChatItems = ["Harry","Timothy","Frankine","Ronald","Sarah","2","Third","4"];
 	
@@ -46,6 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
 	void dispose() {
 		super.dispose();
 		scrollController.dispose();
+		chatListSrollController.dispose();
 	}
 	
 	@override
@@ -93,6 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
 																	color: Colors.white,
 																),
 																child: Column(
+																	mainAxisSize: MainAxisSize.max,
 																	children: <Widget>[
 																		topContainer(),
 																		chatListContainer(),
@@ -117,7 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
 	
 	Widget topContainer() {
 		return Padding(
-			padding: EdgeInsets.all(20.0),
+			padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10),
 			child: Row(
 				mainAxisAlignment: MainAxisAlignment.spaceBetween,
 				children: <Widget>[
@@ -142,10 +146,142 @@ class _MyHomePageState extends State<MyHomePage> {
 	Widget chatListContainer() {
 		return Expanded(
 			child: Container(
-				decoration: BoxDecoration(
-					color: Colors.blueAccent,
+				height: double.infinity,
+				margin: EdgeInsets.symmetric(horizontal: 4.0),
+				child: ListView.separated(
+					primary: false,
+					shrinkWrap: true,
+					physics: BouncingScrollPhysics(),
+					scrollDirection: Axis.vertical,
+					itemCount: lChatItems == null ? 0 : lChatItems.length,
+					controller: chatListSrollController,
+					separatorBuilder: (context, index) => _buildDashWidget(),
+					itemBuilder: (BuildContext context, int index){
+						return Padding(
+							padding: EdgeInsets.symmetric(horizontal: 10.0,vertical: 16.0),
+							child: Row(
+								mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+								children: <Widget>[
+									badgeCount(true),
+									SizedBox(
+										width: 8,
+									),
+									Expanded(
+										child: Row(
+											children: <Widget>[
+												CircleAvatar(
+													radius: 32.0,
+													backgroundImage:NetworkImage(
+														'https://via.placeholder.com/150',
+													),
+													backgroundColor: Colors.transparent,
+												),
+												SizedBox(
+													width: 8,
+												),
+												Flexible(
+													child: Column(
+														mainAxisSize: MainAxisSize.max,
+														crossAxisAlignment: CrossAxisAlignment.start,
+														mainAxisAlignment: MainAxisAlignment.center,
+														children: <Widget>[
+															Text(
+																"James Martin",
+																style: TextStyle(
+																	color: Colors.black,
+																	fontSize: 16.0,
+																	fontWeight: FontWeight.bold,
+																),
+															),
+															SizedBox(height: 4.0,),
+															RichText(
+																textAlign: TextAlign.left,
+																softWrap: true,
+																overflow: TextOverflow.ellipsis,
+																maxLines: 1,
+																text: TextSpan(children:
+																<TextSpan>[
+																	TextSpan(text:"You: ",
+																		style: TextStyle(
+																			color: Colors.blueGrey,
+																			fontWeight: FontWeight.bold,
+																			fontSize: 13.0,
+																		),
+																	),
+																	TextSpan(
+																		text:"Wrap?There's such a long text",
+																		style: TextStyle(
+																			color: Colors.grey,
+																			fontSize: 13.0,
+																		),
+																	),
+																]
+																),
+															),
+														],
+													),
+												),
+											],
+										),
+									),
+									SizedBox(
+										width: 8,
+									),
+									Text(
+										"8.24 PM",
+										style: TextStyle(
+											color: Colors.black,
+											fontSize: 13.0,
+										),
+									),
+								],
+							),
+						);
+					},
 				),
 			),
 		);
+	}
+	
+	
+
+// this is the main widget
+	Widget _buildDashWidget() {
+		
+		double _totalWidth = MediaQuery.of(context).size.width - 40, _dashWidth = 16, _emptyWidth = 16, _dashHeight = 0.5;
+		Color _dashColor = Colors.grey.shade400;
+		
+		return Row(
+			children: List.generate(
+				_totalWidth ~/ (_dashWidth + _emptyWidth),
+					(_) => Container(
+					width: _dashWidth,
+					height: _dashHeight,
+					color: _dashColor,
+					margin: EdgeInsets.only(left: _emptyWidth / 2, right: _emptyWidth / 2),
+				),
+			),
+		);
+	}
+	
+	Widget badgeCount(isShow){
+		return !isShow ?
+			null
+			:
+			Container(
+				padding: EdgeInsets.all(8.0),
+				decoration: BoxDecoration(
+					shape: BoxShape.circle,
+					color: Colors.green,
+				),
+				child: Text(
+					"4",
+					style: TextStyle(
+						color: Colors.white,
+						fontSize: 14.0,
+					),
+				),
+			)
+		;
 	}
 }
